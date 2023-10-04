323aacca494a26500a45ad1db99b2ef3898ba631
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-10-05 02:09:55 +0900

    feat: 기본 코드 추가

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('hi11')
+const cities = require('./cities.json');
+const fs = require('fs');
+
+console.log(cities["서울특별시"].length);
+
+const data = {
+    name: 'gabriel',
+};
+
+const jsonData = JSON.stringify(data, null, 2);
+
+const filename = 'data.json';
+
+fs.writeFile(filename, jsonData, 'utf8', (err) => {
+    if (err) {
+        console.error('파일 생성 실패:', err);
+    } else {
+        console.log('파일 생성!');
+    }
+});
