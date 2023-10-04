e709b01c162395229803bf5a5b011c0ba46e62eb
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-10-05 02:16:32 +0900

    feat: 지역별 지명 갯수 조회

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 const cities = require('./cities.json');
 const fs = require('fs');
-
-console.log(cities["서울특별시"].length);
-
-const data = {
-    name: 'gabriel',
-};
-
-const jsonData = JSON.stringify(data, null, 2);
-
-const filename = 'data.json';
-
-fs.writeFile(filename, jsonData, 'utf8', (err) => {
-    if (err) {
-        console.error('파일 생성 실패:', err);
-    } else {
-        console.log('파일 생성!');
-    }
+const regions = require('./regions.js');
+regions.map((region) => {
+    const {name} = region;
+    const length = cities[name].length;
+    console.log(name, length);
 });
+
+
+// const data = {
+//     name: 'gabriel',
+// };
+//
+// const jsonData = JSON.stringify(data, null, 2);
+//
+// const filename = 'data.json';
+//
+// fs.writeFile(filename, jsonData, 'utf8', (err) => {
+//     if (err) {
+//         console.error('파일 생성 실패:', err);
+//     } else {
+//         console.log('파일 생성!');
+//     }
+// });
