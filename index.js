6328bf8880b53662edb0e71574ca601923974219
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-10-06 13:47:18 +0900

    feat: 지역명 및 위치 생성기 구현

--- index.js
+++ index.js
@@ -1,25 +1,40 @@
 const cities = require('./cities.json');
 const fs = require('fs');
 const regions = require('./regions.js');
+const data = [];
 regions.map((region) => {
-    const {name} = region;
-    const length = cities[name].length;
-    console.log(name, length);
+    data.push(region);
+    cities[region.name].map((city) => {
+        let name = region.name;
+        if(city.a){
+            name+=` ${city.a}`;
+        }
+        if(city.b){
+            name+=` ${city.b}`;
+        }
+        if(city.c){
+            name+=` ${city.c}`;
+        }
+        if(city.d){
+            name+=` ${city.d}`;
+        }
+        data.push({
+            name,
+            latitude: Number(city.lat),
+            longitude: Number(city.lng),
+        });
+    });
 });
 
 
-// const data = {
-//     name: 'gabriel',
-// };
-//
-// const jsonData = JSON.stringify(data, null, 2);
-//
-// const filename = 'data.json';
-//
-// fs.writeFile(filename, jsonData, 'utf8', (err) => {
-//     if (err) {
-//         console.error('파일 생성 실패:', err);
-//     } else {
-//         console.log('파일 생성!');
-//     }
-// });
+const jsonData = JSON.stringify(data, null, 2);
+
+const filename = 'data.json';
+
+fs.writeFile(filename, jsonData, 'utf8', (err) => {
+    if (err) {
+        console.error('파일 생성 실패:', err);
+    } else {
+        console.log('파일 생성!');
+    }
+});
