68326e46f7b9051fbf11fd64372f49431bd3dca9
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-10-12 22:19:34 +0900

    feat: 충전소 정보 생성 기능 구현

--- generators/station.js
+++ generators/station.js
@@ -1,7 +1,41 @@
 import {getConnection} from "../utils/mysql.js";
 
-export const generateStationData = async () => {
-    const [rows, fields] = await getConnection().query('SELECT * from charge_station');
-    console.log(rows);
+const stationMap = new Map();
+const chargerMap = new Map();
 
+const makeStation = async ()=>{
+  const [stationsRow, _stationsFields] = await getConnection().query('SELECT * from charge_station');
+  for(const station of stationsRow) {
+    stationMap.set(station.station_id, station);
+  }
+}
+
+const addChargers = async () => {
+  const [chargersRow, _chargersFields] = await getConnection().query(`
+        SELECT charger.*, charger_status.charger_condition, charger_status.latest_update_time
+        FROM charger
+        INNER JOIN charger_status
+        ON charger.charger_id = charger_status.charger_id
+        AND charger.station_id = charger_status.station_id
+    `);
+  for(const charger of chargersRow) {
+    if(chargerMap.has(charger.station_id)) {
+      chargerMap.get(charger.station_id).push(charger);
+    } else {
+      chargerMap.set(charger.station_id, [charger]);
+    }
+  }
+}
+
+const addChargersToStation = () => {
+  for(const [stationId, chargers] of chargerMap) {
+    const station = stationMap.get(stationId);
+    station.chargers = chargers;
+  }
+}
+
+export const generateStationData = async () => {
+  await makeStation();
+  await addChargers();
+  addChargersToStation();
 }
