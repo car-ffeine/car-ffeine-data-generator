aa8f5927aaa3d1c722c2cfbfa02c1fdbdacce1e5
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-10-12 14:40:13 +0900

    refactor: 파일 구조 개선

---
+++ generators/city.js
@@ -0,0 +1,32 @@
+import {regions} from "../regions.js";
+import cities from "../cities.json" assert {type: "json"};
+import {makeFile} from "../utils/makeFile.js";
+
+export const generateCityData = () => {
+    const data = [];
+    regions.map((region) => {
+        data.push(region);
+        cities[region.name].map((city) => {
+            let name = region.name;
+            if(city.a){
+                name+=` ${city.a}`;
+            }
+            if(city.b){
+                name+=` ${city.b}`;
+            }
+            if(city.c){
+                name+=` ${city.c}`;
+            }
+            if(city.d){
+                name+=` ${city.d}`;
+            }
+            data.push({
+                name,
+                latitude: city.lat==="undefined"?0:Number(city.lat),
+                longitude: city.lng==="undefined"?0:Number(city.lng),
+            });
+        });
+    });
+
+    makeFile(data);
+}
--- index.js
+++ index.js
@@ -1,30 +1,3 @@
-import cities from './cities.json' assert { type: 'json' };
-import {regions} from './regions.js';
-import {makeFile} from "./utils/makeFile.js";
+import {generateCityData} from "./generators/city.js";
 
-const data = [];
-regions.map((region) => {
-    data.push(region);
-    cities[region.name].map((city) => {
-        let name = region.name;
-        if(city.a){
-            name+=` ${city.a}`;
-        }
-        if(city.b){
-            name+=` ${city.b}`;
-        }
-        if(city.c){
-            name+=` ${city.c}`;
-        }
-        if(city.d){
-            name+=` ${city.d}`;
-        }
-        data.push({
-            name,
-            latitude: city.lat==="undefined"?0:Number(city.lat),
-            longitude: city.lng==="undefined"?0:Number(city.lng),
-        });
-    });
-});
-
-makeFile(data);
+generateCityData();
