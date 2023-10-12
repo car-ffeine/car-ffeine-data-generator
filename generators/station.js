a15875b97397c61602e2da5bb589cbe6c09fbe44
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-10-12 22:45:05 +0900

    refactor: 충전소 정보 타입 보정

--- generators/station.js
+++ generators/station.js
@@ -1,4 +1,5 @@
 import {getConnection} from "../utils/mysql.js";
+import {generateRandomData} from "../utils/generateRandomData.js";
 
 const stationMap = new Map();
 const chargerMap = new Map();
@@ -6,7 +7,27 @@ const chargerMap = new Map();
 const makeStation = async ()=>{
   const [stationsRow, _stationsFields] = await getConnection().query('SELECT * from charge_station');
   for(const station of stationsRow) {
-    stationMap.set(station.station_id, station);
+    const newStation = {
+      stationId: station.station_id,
+      stationName: station.station_name,
+      companyName: station.company_name,
+      contact: station.contact,
+      chargers: [], // 다음 단계에서 추가
+      isParkingFree: station.is_parking_free > 0,
+      operatingTime: station.operating_time,
+      address: station.address,
+      detailLocation: station.detail_location,
+      latitude: Number(station.latitude),
+      longitude: Number(station.longitude),
+      isPrivate: station.is_private > 0,
+      totalCount: 0, // 다음 단계에서 추가
+      availableCount: 0, // 다음 단계에서 추가
+      quickChargerCount: 0, // 다음 단계에서 추가
+      stationState: station.station_state,
+      privateReason: station.private_reason,
+      reportCount: generateRandomData([0, 0, Math.floor(Math.random() * 99)]), // 랜덤 데이터 추가
+    }
+    stationMap.set(station.station_id, newStation);
   }
 }
 
@@ -31,6 +52,9 @@ const addChargersToStation = () => {
   for(const [stationId, chargers] of chargerMap) {
     const station = stationMap.get(stationId);
     station.chargers = chargers;
+    station.totalCount = chargers.length;
+    station.availableCount = chargers.filter(charger => charger.charger_condition === 'STANDBY').length;
+    station.quickChargerCount = chargers.filter(charger => Number(charger.capacity) >= 50).length;
   }
 }
 
@@ -38,4 +62,5 @@ export const generateStationData = async () => {
   await makeStation();
   await addChargers();
   addChargersToStation();
+  console.log(stationMap.get("PW013287"));
 }
